a561f749da39a23e572db1e5338a39a584bf996a
josesitanggang <jose.stnggng@gmail.com>
2018-11-28 01:29:16 +0700

    register routes

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const mongoose  = require('mongoose');
 const express = require('express');
 const app = express();
 
+const posts = require('./routes/api/posts');
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
+
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost/enviteramean')
     .then( ()=> console.log('Connected...'))
     .catch( err => console.error('Connecting Failed...',err))
 
+app.use('/api/posts',posts);
+app.use('/api/users',users);
+app.use('/api/profiles',profiles);
 
+app.get('/',(req,res)=>{
+    res.json({status:"succes"});
+})
 
 app.listen(3000,()=>{
     console.log('Running on port 3000');
