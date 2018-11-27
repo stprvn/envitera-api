088cd5ea8da2f19e8f3083678485818039877671
josesitanggang <jose.stnggng@gmail.com>
2018-11-28 04:42:34 +0700

    get profile for public api

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -28,6 +28,54 @@ router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
         .catch(err => res.status(404).json(err));
 })
 
+//  @route  GET api/profile/handle/:handle
+//  @desc   Get profile by handle
+//  @access Public
+router.get('/handle/:handle',(req,res)=>{
+    const errors = {};
+    Profile.findOne({handle:req.params.handle})
+        .populate('user',['name','avatar','email'])
+        .then(profile=>{
+            if(!profile){
+                errors.noprofile = "There is no profile for this user";
+                return res.status(404).json(errors);
+            }
+            return res.json(profile);
+        }).catch(err => res.status(404).json(err))
+})
+
+//  @route  GET api/profile/user/:user_id
+//  @desc   Get profile by user_id
+//  @access Public
+router.get('/user/:user_id',(req,res)=>{
+    const errors = {};
+    Profile.findOne({user:req.params.user_id})
+        .populate('user',['name','avatar','email'])
+        .then(profile=>{
+            if(!profile){
+                errors.noprofile = "There is no profile for this user";
+                return res.status(404).json(errors);
+            }
+            return res.json(profile);
+        }).catch(err => res.status(404).json({noprofile:"There is no profile for this user"}))
+})
+
+//  @route  GET api/profile/all
+//  @desc   Get all profile
+//  @access Public
+router.get('/all',(req,res)=>{
+    const errors = {};
+    Profile.find()
+        .populate('user',['name','avatar','email'])
+        .then(profile=>{
+            if(!profile){
+                errors.noprofile = "There is no profile ";
+                return res.status(404).json(errors);
+            }
+            return res.json(profile);
+        }).catch(err => res.status(404).json({noprofile:"There is no profile"}))
+})
+
 //  @route  POST api/profile
 //  @desc   Create current user profile
 //  @access Private
